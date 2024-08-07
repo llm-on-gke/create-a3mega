ade5b3fbabd8754fa83dd6b3f7687d88904214b3
Rick(Rugui) Chen <rickruguichen@google.com>
2024-08-06 22:34:51 -0400

    dws calendar

--- create_dws_calendar.sh
+++ create_dws_calendar.sh
@@ -6,7 +6,9 @@ PROJECT_NUMBER=9452062936
 ZONE=asia-northeast1-b
 NETWORK_PREFIX=deshaw-test
 PROVISION_MODE=STANDARD #spot or standard
-RESEREVATION=projects/$PROJECT/reservations/a3-mega-us-central1-c #future reservation name
+RESERVATION=projects/$PROJECT/reservations/a3-mega-us-central1-c #future reservation name
+COUNT=1
+
 SYS_SUBNET=$NETWORK_PREFIX-mgmt-sub
 GPU0_SUBNET=$NETWORK_PREFIX-gpunet-1-subnet
 GPU1_SUBNET=$NETWORK_PREFIX-gpunet-2-subnet
@@ -17,24 +19,18 @@ GPU5_SUBNET=$NETWORK_PREFIX-gpunet-6-subnet
 GPU6_SUBNET=$NETWORK_PREFIX-gpunet-7-subnet
 GPU7_SUBNET=$NETWORK_PREFIX-gpunet-8-subnet
 
-#RESERVATION=projects/hpc-toolkit-gsc/reservations/a3-mega-us-central1-c
-#RESERVATION=a3-mega-us-central1-c
-#PLACEMENT_POLICY_NAME=a3-mega-md2-us-central1
-
-# --image-family=projects/hpc-toolkit-gsc/global/images/debian-12-bookworm-v20240515-tcpxo \
-# --image=debian-12-bookworm-tcpxo-v20240515-20240730212714z \
-
 gcloud alpha compute instances bulk create \
-    --count=1 \
+    --count=${COUNT} \
     --name-pattern=a3mega-vms-#### \
     --project=$PROJECT \
-    --image-project=$IMAGE_PROJECT \
+    --image-project=${PROJECT} \
     --image-family=debian-12-bookworm-v20240709-tcpxo \
+    --project=${PROJECT} \
+    --zone=${ZONE} \
     --machine-type=a3-megagpu-8g \
     --maintenance-policy=TERMINATE \
     --restart-on-failure \
     --network-interface=nic-type=GVNIC,subnet=${SYS_SUBNET} \
-    --network-interface=nic-type=GVNIC,subnet=${GPU0_SUBNET} \
     --network-interface=nic-type=GVNIC,subnet=${GPU1_SUBNET} \
     --network-interface=nic-type=GVNIC,subnet=${GPU2_SUBNET} \
     --network-interface=nic-type=GVNIC,subnet=${GPU3_SUBNET} \
@@ -42,18 +38,12 @@ gcloud alpha compute instances bulk create \
     --network-interface=nic-type=GVNIC,subnet=${GPU5_SUBNET} \
     --network-interface=nic-type=GVNIC,subnet=${GPU6_SUBNET} \
     --network-interface=nic-type=GVNIC,subnet=${GPU7_SUBNET} \
+    --network-interface=nic-type=GVNIC,subnet=${GPU8_SUBNET} \
     --metadata=enable-oslogin=true \
-    --provisioning-model=$PROVISION_MODE \
+    --provisioning-model=STANDARD \
     --reservation-affinity=specific \
     --reservation=${RESERVATION} \
-    --zone=$ZONE \
-    --on-host-maintenance=TERMINATE \
-    --maintenance-interval=PERIODIC \
-    --service-account=$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
     --boot-disk-size=200 \
     --boot-disk-type=pd-ssd \
     --labels=goog-ec-src=vm_add-gcloud
-
-
-#--metadata-from-file=startup-script=install_nvidia.sh \
