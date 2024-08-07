d9e1da7bcd944ecdabe930ed619d9b4dcbf26aff
Rick(Rugui) Chen <rickruguichen@google.com>
2024-08-06 22:26:17 -0400

    fix PROJECT variable

--- create_network.sh
+++ create_network.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 set -eu
 
-PROJECT_ID=northam-ce-mlai-tpu
+PROJECT=northam-ce-mlai-tpu
 NETWORK_PREFIX=deshaw-test
 REGION=asia-northeast1
 #Main network and subnet
 gcloud compute networks create $NETWORK_PREFIX-mgmt-net \
-  --project=$PROJECT_ID \
+  --project=$PROJECT \
   --subnet-mode=custom \
   --mtu=8244
 
 gcloud compute networks subnets create $NETWORK_PREFIX-mgmt-sub \
-  --project=$PROJECT_ID \
+  --project=$PROJECT \
   --network=$NETWORK_PREFIX-mgmt-net \
   --region=$REGION \
   --range=192.168.0.0/24
 
 gcloud compute firewall-rules create $NETWORK_PREFIX-mgmt-internal \
- --project=$PROJECT_ID \
+ --project=$PROJECT \
  --network=$NETWORK_PREFIX-mgmt-net \
  --action=ALLOW \
  --rules=tcp:0-65535,udp:0-65535,icmp \
  --source-ranges=192.168.0.0/16
 
 gcloud compute firewall-rules create $NETWORK_PREFIX-mgmt-external-ssh \
- --project=$PROJECT_ID \
+ --project=$PROJECT \
  --network=$NETWORK_PREFIX-mgmt-net \
  --action=ALLOW \
  --rules=tcp:22 \
